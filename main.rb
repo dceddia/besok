a40329e3faaacfb72500628902fe33dc36b49016
Dave Ceddia <dceddia@gmail.com>
2011-08-23 00:33:52 -0400

    adding login functionality

--- main.rb
+++ main.rb
@@ -3,6 +3,7 @@ require 'data_mapper'
 require 'erb'
 require 'rack-flash'
 require 'sinatra/redirect_with_flash'
+require 'bcrypt'
 
 enable :sessions
 use Rack::Flash, :sweep => true
@@ -12,15 +13,23 @@ DataMapper.setup(:default, "sqlite:///#{Dir.pwd}/besok.sqlite3")
 require 'schema'
 
 def shorten(str, max_length)
-    if str.length > max_length
-        str[0..max_length-1]
-    else
-        str
-    end
+    str[0...max_length]
+end
+
+def authorized?
+    session[:authorized]
+end
+
+def authorize!
+    redirect '/login' unless authorized?
+end
+
+def logout!
+    session[:authorized] = false
 end
 
 def add_visit(token, request)
-    page = Page.find(
+    #page = Page.find(
     visit = Visit.new(
         :occurred_at        => Time.now,
         :ip                 => request.ip,
@@ -44,11 +53,30 @@ get '/new' do
     erb :new
 end
 
+get '/login' do
+    redirect '/manage' if authorized?
+    erb :login
+end
+
+post '/login' do
+    redirect '/manage' if authorized?
+
+    page = Page.first(:id => params[:token])
+    puts "--- #{page}"
+    if page and BCrypt::Password.new(page.password) == params[:password]
+        session[:authorized] = true
+        redirect '/manage'
+    else
+        redirect '/login', :error => "Token or Password was incorrect" unless page
+    end
+end
+
 post '/new' do
     page = Page.new(
         :name           => params[:name],
         :url            => params[:url],
-        :description    => params[:description]
+        :description    => params[:description],
+        :password       => BCrypt::Password.create(params[:password])
     )
     if page and page.save
         session[:new_id] = page.id
@@ -69,15 +97,15 @@ get '/welcome' do
     erb :welcome
 end
 
-get '/check' do
-    erb :check
+get '/manage' do
+    erb :manage
 end
 
 get '/visit' do
     redirect '/'
 end
 
-get '/visit/:id' do
-    add_visit(request)
+get '/visit/:token' do |token|
+    add_visit(token, request)
     send_file 'blank.gif'
 end
