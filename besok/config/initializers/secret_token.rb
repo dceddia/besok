0100d459e0303250040e9c6228838a9a83b96c2c
Dave Ceddia <dceddia@gmail.com>
2012-05-10 21:08:05 -0400

    converting to rails

---
+++ besok/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Besok::Application.config.secret_token = 'e331ae3286c3f23265441f0c339b79bf4a7a6a18638cd5a3622ab07ebe47780d7b2f72910c656102fdbf39be3b0ab3caf1e27569d84ae67265ea66eea41fd03b'
