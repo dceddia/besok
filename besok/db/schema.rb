c5266c31a82d4252cbce9431ec76987f8e6509d3
Dave Ceddia <dceddia@gmail.com>
2012-05-19 16:59:28 -0400

    added username, first_name, last_name columns to User model. customized devise to use username instead of email

--- besok/db/schema.rb
+++ besok/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120517000244) do
+ActiveRecord::Schema.define(:version => 20120519204917) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(:version => 20120517000244) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "username"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
