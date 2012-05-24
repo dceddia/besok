ce6be9db8946c625fd358868a7903b26b8c3e4cf
Dave Ceddia <dceddia@gmail.com>
2012-05-23 22:00:07 -0400

    tokens list made more useful, fixed token name generation bug every time it was created

--- app/models/token.rb
+++ app/models/token.rb
@@ -3,12 +3,12 @@ class Token < ActiveRecord::Base
   validates :name, :presence => true
   validates :user_id, :presence => true
   validates :description, :presence => true
-  after_initialize :init
+  before_validation :generate_name
   belongs_to :user
   
-  def init
+  def generate_name
     # So many options...
     # http://blog.logeek.fr/2009/7/2/creating-small-unique-tokens-in-ruby
-    self.name = rand(36**8).to_s
+    self.name = rand(36**8).to_s(36)
   end
 end
