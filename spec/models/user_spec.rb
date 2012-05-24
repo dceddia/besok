34cc6a786352974d9ad227a89abcfea5bbc6ab86
Dave Ceddia <dceddia@gmail.com>
2012-05-24 17:04:56 -0400

    require username for User registration, update wording for usernames instead of emails

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,33 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  def required_attributes
+    {:username => 'user', :email => 'user@user.com', :password => 'password'}
+  end
+  
+  it "should work with username, email, and password" do
+    user = User.new(required_attributes)
+    user.should be_valid
+  end
+  
+  it "should not work with a missing username" do
+    attrs = required_attributes
+    attrs.delete(:username)
+    user = User.new(attrs)
+    user.should_not be_valid
+  end
+  
+  it "should not work with a missing email" do
+    attrs = required_attributes
+    attrs.delete(:email)
+    user = User.new(attrs)
+    user.should_not be_valid
+  end
+
+  it "should not work with a missing password" do
+    attrs = required_attributes
+    attrs.delete(:password)
+    user = User.new(attrs)
+    user.should_not be_valid
+  end
 end
